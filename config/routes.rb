4387917cb2f5181d520302779976cfde5c703937
Ryan Horlacher <horlacherr@gmail.com>
2016-08-16 13:42:42 -0600

    post feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'posts/index'
+
+  # get 'posts/show'
+
+  # get 'posts/new'
+  root 'posts#index'
+  resources :posts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
